2cd427e370a3e2e64aad524c5e9dd591bc50c33a
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2018-10-11 17:05:06 -0500

    Add basic header

---
+++ client/components/MainHeader.vue
@@ -0,0 +1,5 @@
+<template lang="pug">
+  v-toolbar
+    v-toolbar-side-icon
+    v-toolbar-title Barbell Hero
+</template>
---
+++ client/layouts/default.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  #app
+    main-header
+    nuxt
+</template>
+
+<script>
+import MainHeader from "../components/MainHeader.vue";
+export default {
+  components: {
+    MainHeader
+  }
+};
+</script>
+
+<style>
+body {
+  font-family: "Roboto";
+}
+</style>
