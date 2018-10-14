3b5d16b787a2c53a335c65464a06dcfd9320f534
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2018-10-13 19:41:28 -0500

    Add menu with logout button

---
+++ client/components/MainMenu.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  v-menu(offset-y)
+    v-btn(icon slot="activator")
+      v-icon more_vert
+    v-list
+      v-list-tile(@click="logout()")
+        v-list-tile-title Logout
+</template>
+
+<script>
+export default {
+  methods: {
+    async logout() {
+      await this.$store.dispatch("auth/logout");
+      this.$router.push("/login");
+    }
+  }
+};
+</script>
