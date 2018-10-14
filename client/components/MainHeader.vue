1d68c1423beeac2f90ba65fddf5c529dcfb95a9b
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2018-10-13 19:41:18 -0500

    Fix login

--- client/components/MainHeader.vue
+++ client/components/MainHeader.vue
@@ -4,12 +4,16 @@
     v-toolbar(color="primary base" dark fixed app)
       v-toolbar-side-icon(@click.stop="$emit('drawer:toggle')")
       v-toolbar-title Barbell Hero
+      v-spacer
+      main-menu
 </template>
 
 <script>
+import MainMenu from "../components/MainMenu.vue";
 import MainNavigation from "../components/MainNavigation.vue";
 export default {
   components: {
+    MainMenu,
     MainNavigation
   },
   methods: {
--- client/middleware/auth.js
+++ client/middleware/auth.js
@@ -1,7 +1,11 @@
 export default async function(context) {
   const { store, redirect, route } = context;
   const { auth } = store.state;
-  await store.dispatch("auth/authenticate");
+  try {
+    await store.dispatch("auth/authenticate");
+  } catch (e) {
+    /* no JWT found */
+  }
   const authenticated = store.getters["auth/authenticated"];
   if (!auth.publicPages.includes(route.name) && !authenticated) {
     return redirect("/login");
