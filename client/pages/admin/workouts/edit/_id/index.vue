31aa0c033644f421d423fa4c153a4c0e6f8dee70
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2018-10-18 22:13:41 -0500

    Simplify amrap filter

--- client/pages/admin/workouts/edit/_id/index.vue
+++ client/pages/admin/workouts/edit/_id/index.vue
@@ -5,7 +5,7 @@
         div(slot="nav-item" slot-scope="{ item }")
           v-list-tile-content
             v-list-tile-title {{item["movement.name"]}}
-            v-list-tile-sub-title {{item.repetitions | amrap(item)}}
+            v-list-tile-sub-title {{item | amrap}}
 </template>
 
 <script>
--- client/plugins/filters.js
+++ client/plugins/filters.js
@@ -1,9 +1,6 @@
 import Vue from "vue";
 
-Vue.filter("amrap", function(value, movementSet) {
-  if (!movementSet) {
-    return value;
-  }
+Vue.filter("amrap", function(movementSet) {
   let result = `${movementSet.repetitions}`;
   if (movementSet.amrap) {
     result += "+";
